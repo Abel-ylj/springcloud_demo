0a374e94bc74a745095851f6b3386324decd1d14
ylj <aboutlistener@163.com>
2021-01-24 17:39:31 +0800

    消费工程 从eureka获取服务列表，rest请求服务

--- springcloud/consumer/src/main/java/cn/ylj/consumer/controller/ConsumerController.java
+++ springcloud/consumer/src/main/java/cn/ylj/consumer/controller/ConsumerController.java
@@ -1,12 +1,15 @@
 package cn.ylj.consumer.controller;
 
 import cn.ylj.consumer.entity.User;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author : yanglujian
@@ -18,10 +21,21 @@ public class ConsumerController {
 
     @Resource
     private RestTemplate restTemplate;
+    @Resource
+    private DiscoveryClient discoveryClient;
+
 
     @RequestMapping("/{uId}")
     public User findById(@PathVariable("uId") Integer uId){
-        User user = restTemplate.getForObject("http://localhost:9091/user/" + uId, User.class);
+        //获取叫user-serivce的服务(高可用的话有多个)
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+        //选取第一个
+        ServiceInstance serviceInstance = instances.get(0);
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/user/" + uId;
+
+//        User user = restTemplate.getForObject("http://localhost:9091/user/" + uId, User.class);
+        //从注册中心获取服务，解耦，不直接依赖具体
+        User user = restTemplate.getForObject(url, User.class);
         System.out.println(user);
         return user;
     }
