1dace6c645f9f5593a325ef6acae94d157929d23
ylj <aboutlistener@163.com>
2021-01-24 17:18:26 +0800

    服务注册到eureka

--- springcloud/user-service/src/main/java/cn/ylj/user/UserApplication.java
+++ springcloud/user-service/src/main/java/cn/ylj/user/UserApplication.java
@@ -2,10 +2,13 @@ package cn.ylj.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("cn.ylj.user.mapper")
+@EnableDiscoveryClient //开启发现功能
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
